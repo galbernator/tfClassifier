752c692bb6ca399c8a138cc8e190ff5ca87fec3c
Steve Galbraith <steve@stevegalbraith.com>
2018-03-19 11:45:15 -0400

    Update file path info in model classification

--- image_classification/retrain_model_classifier.py
+++ image_classification/retrain_model_classifier.py
@@ -1,5 +1,6 @@
 import sys
 import tensorflow as tf
+import os
 
 # change this as you see fit
 image_path = sys.argv[1]
@@ -8,11 +9,13 @@ image_path = sys.argv[1]
 image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
 # Loads label file, strips off carriage return
+label_file_path = os.path.expanduser("YOUR_FILE_PATH/labels.txt")
 label_lines = [line.rstrip() for line
-                   in tf.gfile.GFile("./labels.txt")]
+                   in tf.gfile.GFile(label_file_path)]
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("./output.pb", 'rb') as f:
+output_file_path = os.path.expanduser("YOUR_FILE_PATH/output.pb")
+with tf.gfile.FastGFile(output_file_path, 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
